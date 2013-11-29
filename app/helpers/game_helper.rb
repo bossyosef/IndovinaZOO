74844b74467a8320e944c18070e51ad4f25b3d7d
bossyosef <bossyosef@gmail.com>
2013-11-29 10:54:28 +0100

    helper path non funziona

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,8 +1,2 @@
 module GameHelper
-  def form_path
-    if params[:id] < params[:numeroquiz]
-      return "/quiz/"+(params[:id].to_i+1).to_s
-	elsif params[:id] == params[:numeroquiz]
-	  return "/result"
-  end
 end
