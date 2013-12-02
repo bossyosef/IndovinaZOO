ca7af065857fb985907e3a32d2e3e5b59c78b57a
bossyosef <bossyosef@gmail.com>
2013-12-02 10:33:31 +0100

    modifiche importanti 1

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,11 +6,13 @@ class GameController < ApplicationController
   end
 
   def quiz
-	#if (params[:risultati])
-	#  @quiz, @risposta = Quiz.random(params[:risultati])
-	#else
-	#  @quiz_id_arr, @animali_arr, @risposte_arr, @punteggi_arr=random_quiz_array(params[:numeroquiz])
-	#end
+	if(params[:quiz_id_arr])
+	  @immagine1 = Quiz.find
+	  @immagine2 = 
+	  @verso = Quiz.find(params[:quiz_id_arr][params[:id].to_i-1])
+	else
+	  @quiz_id_arr, @animali_arr, @risposte_arr, @punteggi_arr = Quiz.random_quiz_array(params[:numeroquiz])
+	end
   end
 
   def result
