80decfb2e220ea2a02ac39d6693242df8427ff06
bossyosef <bossyosef@gmail.com>
2014-01-03 16:57:51 +0100

    passa i dati anche la prima volta, furbo

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,6 +11,8 @@ class GameController < ApplicationController
 	  @verso = Animal.get_cry(params[:animali_arr][params[:id].to_i-1])
 	else
 	  @quiz_id_arr, @animali_arr, @risposte_arr, @punteggi_arr = Quiz.random_quiz_array(params[:numeroquiz])
+	  @animale1, @animale2 = Quiz.get_animals_from_id(@quiz_id_arr[0])
+	  @verso = Animal.get_cry(@animali_arr[0])
 	end
   end
 
