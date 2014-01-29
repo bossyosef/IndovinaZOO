b58ae5178ded7033fd16e6a3ef3763acb3b89965
bossyosef <bossyosef@gmail.com>
2014-01-29 17:10:24 +0100

    transizione a gon 1-?

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,6 +4,9 @@ class GameController < ApplicationController
 
   def settings
 	@livello_alto, @quantita_quiz, @numero_quiz = Quiz.levels_hash
+	gon.livello_alto = @livello_alto
+	gon.quantita_quiz = @quantita_quiz
+	gon.numero_quiz = @numero_quiz.to_json
   end
 
   def quiz
